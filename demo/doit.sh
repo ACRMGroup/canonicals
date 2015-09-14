88637bd786e7fba8858e8e670417e553d783cc6b
AndrewCRMartin <andrew@bioinf.org.uk>
2015-09-14 10:16:00 +0100

    V3.9 - completed demo script

--- demo/doit.sh
+++ demo/doit.sh
@@ -1,6 +1,10 @@
 # Setup
 makeclan=./scripts/makeclan.pl
 clan=../src/clan
+findsdrs=../src/findsdrs
+
+# Uncomment this line for testing on a small set:
+#limit='-limit=10'   
 
 # Grab and unpack the non-redundant Chothia-numbered PDB files of antibodies
 wget www.bioinf.org.uk/abs/abdb/Data/NR_CombinedAb_Chothia.tar.bz2
@@ -8,20 +12,36 @@ tar jxvf NR_CombinedAb_Chothia.tar.bz2
 rm NR_CombinedAb_Chothia.tar.bz2
 
 # Create the Clan files
-$makeclan -limit=10 NR_CombinedAb_Chothia L1
-#$makeclan -limit=10 NR_CombinedAb_Chothia L2
-#$makeclan -limit=10 NR_CombinedAb_Chothia L3
-#$makeclan -limit=10 NR_CombinedAb_Chothia H1
-#$makeclan -limit=10 NR_CombinedAb_Chothia H2
+$makeclan $limit NR_CombinedAb_Chothia L1
+$makeclan $limit NR_CombinedAb_Chothia L2
+$makeclan $limit NR_CombinedAb_Chothia L3
+$makeclan $limit NR_CombinedAb_Chothia H1
+$makeclan $limit NR_CombinedAb_Chothia H2
 
 # Make a results directory
 mkdir -p results
 
 # Run CLAN
-$clan L1.clan
-$clan L2.clan
-$clan L3.clan
-$clan H1.clan
-$clan H2.clan
+for loop in L1 L2 L3 H1 H2
+do
+   echo "Analyzing clusters for CDR $loop"
+   $clan $loop.clan
+done
 
+# Run findsdrs
+for loop in L1 L2 L3 H1 H2
+do
+   echo -n "Finding key residues for CDR $loop..."
+   $findsdrs -k results/${loop}_clan.out results/${loop}.sdrs 2>results/${loop}.log
+   echo "done"
+done
+
+# Remove the solvent accessibility files
+\rm *.sa
+
+echo " "
+echo " "
+
+echo "Final results are in results/??.sdrs"
+echo "Log files are in results/??.log"
 
