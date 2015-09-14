bdb6c9e0bbbe853b443135c1b14fe9f6a256d542
AndrewCRMartin <andrew@bioinf.org.uk>
2015-09-14 10:38:42 +0100

    Added to cleanup script in demo

--- demo/clean.sh
+++ demo/clean.sh
@@ -1,3 +1,3 @@
-rm -rf NR_CombinedAb_Chothia NR_CombinedAb_Chothia.tar.bz2
-rm -rf results
-rm *.clan
+\rm -rf NR_CombinedAb_Chothia NR_CombinedAb_Chothia.tar.bz2
+\rm -rf results
+\rm -f *.clan *.sa *.pdb
