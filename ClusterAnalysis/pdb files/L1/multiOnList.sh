76a8aca8cbf2669c6faca4d7e54fd181aa175397
sonulchoi217 <sonulchoi217@gmail.com>
2015-12-26 16:43:17 +0000

    Added ClusterAnalysis

---
+++ ClusterAnalysis/pdb files/L1/multiOnList.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+for file in *.pdb
+do pdbgetzone L24 L34 $file "$file"_cut.pdb
+done
+
+read -p 'Number of clusters: ' clusterNumber
+currentNumber=1
+
+while [ $currentNumber -le $clusterNumber ]
+do
+profit <<EOF
+multi $currentNumber
+atoms ca
+fit
+mwrite $currentNumber
+EOF
+
+cat *.$currentNumber > $currentNumber.pdb
+rm *.$currentNumber
+pdbchain $currentNumber.pdb $currentNumber.chain.pdb
+currentNumber=$(( currentNumber+1))
+done
+
+rm *_cut.pdb
+
+
+
